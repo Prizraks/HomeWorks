4e70c77d703ffde2a3f216595ed9374966a6e53f
Vladislav Murin <vlad23475@gmail.com>
2024-05-06 12:49:33 +0300

    Добавил  набор тестов для расчета корней квдратного уровнения(task-1)

---
+++ task-1/Task1/Exceptions/CoeffNanNumberException.cs
@@ -0,0 +1,21 @@
+﻿namespace Task1.Exceptions
+{
+    using System;
+
+    public class CoeffNanNumberException : Exception
+    {
+        public CoeffNanNumberException()
+        {
+        }
+
+        public CoeffNanNumberException(string message)
+            : base(message)
+        {
+        }
+
+        public CoeffNanNumberException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
