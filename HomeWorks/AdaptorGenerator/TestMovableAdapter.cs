30569f60942ab9175f64e1fd2d259c9d919c94be
Vladislav Murin <vlad23475@gmail.com>
2024-07-06 19:44:05 +0300

    Добавил ДЗ №6(task-6)

---
+++ HomeWorks/AdaptorGenerator/TestMovableAdapter.cs
@@ -0,0 +1,30 @@
+﻿using System.Numerics;
+
+using UserObjects.Interfaces;
+using UserObjects.Objects;
+
+using HomeWorks.IocContainer;
+using HomeWorks.Interfaces;
+
+namespace HomeWorks.AdaptorGenerator
+{
+    internal class TestMovableAdapter(UserObject obj) : IMovable
+    {
+        UserObject obj = obj;
+
+        public Vector2 GetPosition()
+        {
+            return IoC.Resolve<Vector2>("Spaceship.Operations.IMovable:position.get", obj);
+        }
+
+        public Vector2 GetVelocity()
+        {
+            return IoC.Resolve<Vector2>("Spaceship.Operations.IMovable:velocity.get", obj);
+        }
+
+        public void SetPosition(Vector2 newValue)
+        {
+            IoC.Resolve<ICommand>("Spaceship.Operations.IMovable:position.set", obj, newValue).Execute();
+        }
+    }
+}
